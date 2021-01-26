6dab1818e24fb9111c4f6968860efc6be08409f5
Lex Sulzer <lex@dataspects.com>
2021-01-26 12:06:16 +0100

    Removed CanastaInstanceSettings.sh

--- CanastaInstanceSettings.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Configure ########################
-
-CANASTA_INSTANCE_ROOT=/home/dserver/mediawiki-canasta # No trailing / !
-CANASTA_INSTANCE_ROOT_OWNER=dserver
-CURRENT_CANASTA_ARCHIVE=mediawiki-root-w-folder-1.35.0-3.2.1.tar.gz
