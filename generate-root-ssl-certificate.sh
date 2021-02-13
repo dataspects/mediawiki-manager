fe367b8fe137010d75db59de31551a7652a78f85
Lex Sulzer <lex@dataspects.com>
2021-02-13 17:55:30 +0100

    Enabled localhost SSL

---
+++ generate-root-ssl-certificate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ./CanastaInstanceSettings.env
+
+####################################
+
+# https://letsencrypt.org/docs/certificates-for-localhost/
+
+openssl req -x509 -out localhost.crt -keyout localhost.key \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
--- install-canasta-version.sh
+++ install-canasta-version.sh
@@ -7,9 +7,6 @@ source ./CanastaInstanceSettings.env
 MEDIAWIKI_ROOT_FOLDER=$CANASTA_INSTANCE_ROOT/mediawiki_root
 
 APACHE_CONTAINER_NAME=mediawiki_canasta
-MYSQL_HOST=127.0.0.1
-DATABASE_NAME=mediawiki
-MYSQL_USER=mediawiki
 
 requiredFiles=( "docker-compose.yml" "$CURRENT_CANASTA_ARCHIVE" )
 for file in "${requiredFiles[@]}"
@@ -24,24 +21,28 @@ echo "Run docker-compose..."
 sudo -S docker-compose --env-file ./CanastaInstanceSettings.env down \
   && sudo -S docker-compose --env-file ./CanastaInstanceSettings.env up -d \
   && sudo -S chown -R $CANASTA_INSTANCE_ROOT_OWNER:www-data mediawiki_root
-sleep 5
+sleep 1
 
 echo "Extract..."
 mkdir --parents $MEDIAWIKI_ROOT_FOLDER/w
 tar -xzf $CANASTA_INSTANCE_ROOT/$CURRENT_CANASTA_ARCHIVE -C $MEDIAWIKI_ROOT_FOLDER/w
-sleep 5
+sleep 1
 
 echo "Ensure permissions..."
 sudo chown -R www-data $MEDIAWIKI_ROOT_FOLDER/w/images
-sleep 5
+sleep 1
 
 echo "Set domain name..."
-sed -i "s/^\$wgServer.*;/\$wgServer = '$CANASTA_DOMAIN_PROTOCOL:\/\/$CANASTA_DOMAIN_NAME:$CANASTA_DOMAIN_PORT';/g" mediawiki_root/w/LocalSettings.php
-sleep 5
+sed -i "s/^\$wgServer.*;/\$wgServer = '$CANASTA_DOMAIN_NAME';/g" mediawiki_root/w/LocalSettings.php
+sleep 1
 
 echo "Set database password..."
 sed -i "s/^\$wgDBpassword.*;/\$wgDBpassword = '$WG_DB_PASSWORD';/g" mediawiki_root/w/LocalSettings.php
-sleep 5
+sleep 1
+
+echo "Set database server..."
+sed -i "s/^\$wgDBserver.*;/\$wgDBserver = '$MYSQL_HOST';/g" mediawiki_root/w/LocalSettings.php
+sleep 1
 
 # FIXME: Wait for MariaDB to be ready...
 
@@ -49,15 +50,15 @@ echo "Create database and user..."
 sudo -S docker exec $APACHE_CONTAINER_NAME bash -c \
   "mysql -h $MYSQL_HOST -u root -p$MARIADB_ROOT_PASSWORD \
   -e \" CREATE DATABASE $DATABASE_NAME;
-        CREATE USER '$MYSQL_USER'@'$MYSQL_HOST' IDENTIFIED BY '$WG_DB_PASSWORD';
-        GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'$MYSQL_HOST'; \""
-sleep 5
+        CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$WG_DB_PASSWORD';
+        GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$MYSQL_USER'@'%'; \""
+sleep 1
 
 echo "Import database..."
 sudo -S docker exec $APACHE_CONTAINER_NAME bash -c \
   "mysql -h $MYSQL_HOST -u $MYSQL_USER -p$WG_DB_PASSWORD \
   mediawiki < /var/www/html/w/db.sql"
-sleep 5
+sleep 1
 
 echo "Update..."
 sudo -S docker exec $APACHE_CONTAINER_NAME /bin/bash -c \
