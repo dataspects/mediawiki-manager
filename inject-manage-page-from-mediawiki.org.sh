d57353e56fd5342883768f0a856b55e2d48c6e7c
Lex Sulzer <lex@dataspects.com>
2021-02-18 10:51:49 +0100

    Switched to Project_Canasta/Infrastructure_development section 1

--- inject-manage-page-from-mediawiki.org.sh
+++ inject-manage-page-from-mediawiki.org.sh
@@ -7,8 +7,8 @@ source ./CanastaInstanceSettings.env
 source ./scripts/utils.sh
 
 WIKIAPI=https://www.mediawiki.org/w/api.php
-TITLE=Project_Canasta/Infrastructure_solution
-SECTION=5
+TITLE=Project_Canasta/Infrastructure_development
+SECTION=1
 source ./scripts/mediawiki-get-wikitext-from-api.sh
 
 source ./scripts/mediawiki-login-for-edit.sh
